8b8544484cb0bafea3432673ed9c856bed6d0668
xiaoxing <1531553694@qq.com>
2017-06-07 11:25:49 +0800

    no message

--- Android5/app/src/main/java/com/xiao/android5/MainActivity.java
+++ Android5/app/src/main/java/com/xiao/android5/MainActivity.java
@@ -65,6 +65,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tab.addTab(tab.newTab().setText("测试专用数据2"));
         tab.addTab(tab.newTab().setText("测试专用数据3"));
         String text1 = "测试3333333";
+        String text4 = "测试344444444444444";
         tab.addTab(tab.newTab().setText("测试专用数据123"));
         tab.addTab(tab.newTab().setText("测试专用数据4"));
         vp = (ViewPager) findViewById(R.id.vp);
